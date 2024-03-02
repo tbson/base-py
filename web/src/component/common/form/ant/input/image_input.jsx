99f78a05d40e47cdc5fbc1644c92941851ece285
Son Tran <tbson87@gmail.com>
2024-03-02 21:43:17 +0700

    Add image url to image input.

--- web/src/component/common/form/ant/input/image_input.jsx
+++ web/src/component/common/form/ant/input/image_input.jsx
@@ -1,6 +1,7 @@
 import * as React from 'react';
 import { useState } from 'react';
-import { PlusOutlined } from '@ant-design/icons';
+import { t } from 'ttag';
+import { PlusOutlined, FileImageOutlined } from '@ant-design/icons';
 import { Upload } from 'antd';
 
 const getBase64 = (img, callback) => {
@@ -19,7 +20,6 @@ const getBase64 = (img, callback) => {
  * @returns {ReactElement}
  */
 export default function ImageInput({ value, onChange }) {
-    console.log(value)
     const [imageUrl, setImageUrl] = useState(value);
     const uploadButton = (
         <button
@@ -46,27 +46,37 @@ export default function ImageInput({ value, onChange }) {
             setImageUrl(imageUrl);
         });
         return false;
-    }
+    };
 
     return (
-        <Upload
-            name="avatar"
-            listType="picture-card"
-            className="avatar-uploader"
-            showUploadList={false}
-            beforeUpload={handleChange}
-        >
+        <div>
+            <Upload
+                name="avatar"
+                listType="picture-card"
+                className="avatar-uploader"
+                showUploadList={false}
+                beforeUpload={handleChange}
+            >
+                {imageUrl ? (
+                    <img
+                        src={imageUrl}
+                        alt="avatar"
+                        style={{
+                            width: '100%'
+                        }}
+                    />
+                ) : (
+                    uploadButton
+                )}
+            </Upload>
+
             {imageUrl ? (
-                <img
-                    src={imageUrl}
-                    alt="avatar"
-                    style={{
-                        width: '100%'
-                    }}
-                />
-            ) : (
-                uploadButton
-            )}
-        </Upload>
+                <a href={imageUrl} target="_blank">
+                    <FileImageOutlined />
+                    &nbsp;&nbsp;
+                    <span>{t`Image URL`}</span>
+                </a>
+            ) : null}
+        </div>
     );
 }
